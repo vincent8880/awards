eceee5959ed6719dcf102f126137228766bc0897
vincent <ododovincent54@gmail.com>
2019-07-31 15:19:07 +0300

    created a models page with name, description, url and homepage

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField(blank=True)
+    homepage = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def get_all(cls):
+        projects = Project.objects.all()
+        return projects
