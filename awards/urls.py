263ba86560a5e25db47047ba3e1618d242aba062
vincent <ododovincent54@gmail.com>
2019-08-01 09:42:59 +0300

    installed simplejson to use in converting api data

--- awards/urls.py
+++ awards/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     url('^$',views.home,name = 'home'),
     url(r'^repos/$', views.all_repos, name='repos')
 ]
-]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
