9237b93d5e63a9fe5c017edbbc6166f15620e44b
vincent <ododovincent54@gmail.com>
2019-08-01 10:30:21 +0300

    insalled pyuploadcare to import imagefield to use in models

--- awards/models.py
+++ awards/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     url = models.URLField(blank=True)
-    homepage = models.URLField(blank=True)
+    homepage = ImageField(blank=True, manual_crop='800x800')
 
     def __str__(self):
         return self.name
