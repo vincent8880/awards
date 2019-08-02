5a3ec1998cffc4c986c29b6afdab2233f3011e59
vincent <ododovincent54@gmail.com>
2019-08-02 11:29:45 +0300

    added footer to my base.html so as to link to all other pages

--- awards/models.py
+++ awards/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     description = models.TextField(blank=True)
     url = models.URLField(blank=True)
     homepage = ImageField(blank=True, manual_crop='800x800')
-
+   
     def __str__(self):
         return self.name
     
@@ -15,3 +15,20 @@ class Project(models.Model):
     def get_all(cls):
         projects = Project.objects.all()
         return projects
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+#     pic = ImageField(blank=True, manual_crop="")
+#     bio = models.CharField(default="Hi!", max_length = 30)
+
+#     @receiver(post_save, sender=User)
+#     def create_user_profile(sender, instance, created, **kwargs):
+#         if created:
+#             Profile.objects.create(user=instance)
+
+#     @receiver(post_save, sender=User)
+#     def save_user_profile(sender, instance, **kwargs):
+#         instance.profile.save()
+    
+#     @classmethod
+#     def search_user(cls,name):
+#         return User.objects.filter(username__icontains = name)
